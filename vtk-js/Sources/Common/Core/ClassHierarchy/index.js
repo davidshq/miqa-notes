66843158c491f7107732f8cb70fc9f82c22202b0
Dave Mackey <dave@davemackey.net>
2022-11-15 12:21:13 -0500

    Add additioanl vtk.js source

---
+++ vtk-js/Sources/Common/Core/ClassHierarchy/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable prefer-rest-params */
+export default class ClassHierarchy extends Array {
+    push() {
+        for (let i = 0; i < arguments.length; i++) {
+            if (!this.includes(arguments[i])) {
+                super.push(arguments[i]);
+            }
+        }
+
+        return this.length;
+    }
+}
