a3e03fc380b97e0132c38d001d918623cd61dc80
Dave Mackey <dave@davemackey.net>
2023-01-25 11:42:10 -0500

    Reset proxy

--- vtk-js/miqa-implementation/code/vtk/proxy.js
+++ vtk-js/miqa-implementation/code/vtk/proxy.js
@@ -20,153 +20,148 @@ import proxyViewRepresentationMapping from './proxyViewRepresentationMapping';
 
 const { createProxyDefinition, activateOnCreate } = ConfigUtils;
 
-let defaultViewOptions = [
-  {
-    type: 'application',
-    link: 'AnnotationOpacity',
-    property: 'annotationOpacity',
-    updateOnBind: true,
-  },
-  {
-    type: 'application',
-    link: 'OrientationAxesVisibility',
-    property: 'orientationAxesVisibility',
-    updateOnBind: true,
-  },
-  {
-    type: 'application',
-    link: 'OrientationAxesPreset',
-    property: 'presetToOrientationAxes',
-    updateOnBind: true,
-  },
-  {
-    type: 'application',
-    link: 'OrientationAxesType',
-    property: 'orientationAxesType',
-    updateOnBind: true,
-  },
-];
-
-let definitions = {
-  Proxy: {
-    LookupTable: createProxyDefinition(vtkLookupTableProxy, [], [], {
-      presetName: 'Default (Cool to Warm)',
-    }),
-    PiecewiseFunction: createProxyDefinition(vtkPiecewiseFunctionProxy),
-  },
-  Sources: {
-    TrivialProducer: activateOnCreate(createProxyDefinition(vtkProxySource)),
-    Contour: proxyFilter.Contour,
-  },
-  Representations: {
-    Geometry: createProxyDefinition(
-      vtkGeometryRepresentationProxy,
-      proxyUI.Geometry,
-      proxyLinks.Geometry,
-    ),
-    Skybox: createProxyDefinition(
-      vtkSkyboxRepresentationProxy,
-      proxyUI.Skybox,
-      proxyLinks.Skybox,
-    ),
-    Slice: createProxyDefinition(
-      vtkSliceRepresentationProxy,
-      proxyUI.Slice,
-      proxyLinks.Slice,
-    ),
-    SliceX: createProxyDefinition(
-      vtkSliceRepresentationProxy,
-      proxyUI.Slice,
-      [{ link: 'SliceX', property: 'slice', updateOnBind: true }].concat(
-        proxyLinks.Slice,
-      ),
-    ),
-    SliceY: createProxyDefinition(
-      vtkSliceRepresentationProxy,
-      proxyUI.Slice,
-      [{ link: 'SliceY', property: 'slice', updateOnBind: true }].concat(
-        proxyLinks.Slice,
-      ),
-    ),
-    SliceZ: createProxyDefinition(
-      vtkSliceRepresentationProxy,
-      proxyUI.Slice,
-      [{ link: 'SliceZ', property: 'slice', updateOnBind: true }].concat(
-        proxyLinks.Slice,
-      ),
-    ),
-    Volume: createProxyDefinition(
-      vtkVolumeRepresentationProxy,
-      proxyUI.Volume,
-      proxyLinks.Volume,
-    ),
-    Molecule: createProxyDefinition(
-      vtkMoleculeRepresentationProxy,
-      proxyUI.Molecule,
-      proxyLinks.Molecule,
-    ),
-    Glyph: createProxyDefinition(
-      vtkGlyphRepresentationProxy,
-      proxyUI.Glyph,
-      proxyLinks.Glyph,
-    ),
-  },
-  Views: {
-    View3D: createDefaultView(vtkView, proxyUI.View3D),
-    View2D: createDefaultView(vtk2DView, proxyUI.View2D),
-    View2D_X: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 0 }),
-    View2D_Y: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 1 }),
-    View2D_Z: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 2 }),
-    ScreenshotView2D_x: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 0 }),
-    ScreenshotView2D_y: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 1 }),
-    ScreenshotView2D_z: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 2 }),
-  },
-}
-
-let representations = {
-  View3D: proxyViewRepresentationMapping.View3D,
-  View2D: proxyViewRepresentationMapping.View2D,
-  View2D_X: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceX' },
-  },
-  View2D_Y: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceY' },
-  },
-  View2D_Z: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceZ' },
-  },
-  ScreenshotView2D_x: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceX' },
-  },
-  ScreenshotView2D_y: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceY' },
-  },
-  ScreenshotView2D_z: {
-    ...proxyViewRepresentationMapping.View2D,
-    vtkImageData: { name: 'SliceZ' },
-  },
-}
-
 function createDefaultView(classFactory, ui, options, props) {
   return activateOnCreate(
     createProxyDefinition(
       classFactory,
       ui,
-      defaultViewOptions,
+      [
+        {
+          type: 'application',
+          link: 'AnnotationOpacity',
+          property: 'annotationOpacity',
+          updateOnBind: true,
+        },
+        {
+          type: 'application',
+          link: 'OrientationAxesVisibility',
+          property: 'orientationAxesVisibility',
+          updateOnBind: true,
+        },
+        {
+          type: 'application',
+          link: 'OrientationAxesPreset',
+          property: 'presetToOrientationAxes',
+          updateOnBind: true,
+        },
+        {
+          type: 'application',
+          link: 'OrientationAxesType',
+          property: 'orientationAxesType',
+          updateOnBind: true,
+        },
+      ],
       options,
       props,
     ),
   );
 }
 
+// ----------------------------------------------------------------------------
 export default {
-  definitions: definitions,
-  representations: representations,
+  definitions: {
+    Proxy: {
+      LookupTable: createProxyDefinition(vtkLookupTableProxy, [], [], {
+        presetName: 'Default (Cool to Warm)',
+      }),
+      PiecewiseFunction: createProxyDefinition(vtkPiecewiseFunctionProxy),
+    },
+    Sources: {
+      TrivialProducer: activateOnCreate(createProxyDefinition(vtkProxySource)),
+      Contour: proxyFilter.Contour,
+    },
+    Representations: {
+      Geometry: createProxyDefinition(
+        vtkGeometryRepresentationProxy,
+        proxyUI.Geometry,
+        proxyLinks.Geometry,
+      ),
+      Skybox: createProxyDefinition(
+        vtkSkyboxRepresentationProxy,
+        proxyUI.Skybox,
+        proxyLinks.Skybox,
+      ),
+      Slice: createProxyDefinition(
+        vtkSliceRepresentationProxy,
+        proxyUI.Slice,
+        proxyLinks.Slice,
+      ),
+      SliceX: createProxyDefinition(
+        vtkSliceRepresentationProxy,
+        proxyUI.Slice,
+        [{ link: 'SliceX', property: 'slice', updateOnBind: true }].concat(
+          proxyLinks.Slice,
+        ),
+      ),
+      SliceY: createProxyDefinition(
+        vtkSliceRepresentationProxy,
+        proxyUI.Slice,
+        [{ link: 'SliceY', property: 'slice', updateOnBind: true }].concat(
+          proxyLinks.Slice,
+        ),
+      ),
+      SliceZ: createProxyDefinition(
+        vtkSliceRepresentationProxy,
+        proxyUI.Slice,
+        [{ link: 'SliceZ', property: 'slice', updateOnBind: true }].concat(
+          proxyLinks.Slice,
+        ),
+      ),
+      Volume: createProxyDefinition(
+        vtkVolumeRepresentationProxy,
+        proxyUI.Volume,
+        proxyLinks.Volume,
+      ),
+      Molecule: createProxyDefinition(
+        vtkMoleculeRepresentationProxy,
+        proxyUI.Molecule,
+        proxyLinks.Molecule,
+      ),
+      Glyph: createProxyDefinition(
+        vtkGlyphRepresentationProxy,
+        proxyUI.Glyph,
+        proxyLinks.Glyph,
+      ),
+    },
+    Views: {
+      View3D: createDefaultView(vtkView, proxyUI.View3D),
+      View2D: createDefaultView(vtk2DView, proxyUI.View2D),
+      View2D_X: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 0 }),
+      View2D_Y: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 1 }),
+      View2D_Z: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 2 }),
+      ScreenshotView2D_x: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 0 }),
+      ScreenshotView2D_y: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 1 }),
+      ScreenshotView2D_z: createDefaultView(vtk2DView, proxyUI.View2D, { axis: 2 }),
+    },
+  },
+  representations: {
+    View3D: proxyViewRepresentationMapping.View3D,
+    View2D: proxyViewRepresentationMapping.View2D,
+    View2D_X: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceX' },
+    },
+    View2D_Y: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceY' },
+    },
+    View2D_Z: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceZ' },
+    },
+    ScreenshotView2D_x: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceX' },
+    },
+    ScreenshotView2D_y: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceY' },
+    },
+    ScreenshotView2D_z: {
+      ...proxyViewRepresentationMapping.View2D,
+      vtkImageData: { name: 'SliceZ' },
+    },
+  },
   filters: {
     vtkPolyData: [],
     vtkImageData: ['Contour'],
